f83387accde3871b96d6f2f0702350adf73da244
zhibi <1017866168@qq.com>
2019-03-29 11:05:53 +0800

    地址选择页面

--- pages/select-address/index.js
+++ pages/select-address/index.js
@@ -1,54 +1,49 @@
 const WXAPI = require('../../wxapi/main')
-const app = getApp()
 Page({
   data: {
     addressList: []
   },
 
-  selectTap: function(e) {
-    var id = e.currentTarget.dataset.id;
+  selectTap(e) {
+    const id = e.currentTarget.dataset.id;
     WXAPI.updateAddress({
       token: wx.getStorageSync('token'),
       id: id,
       isDefault: 'true'
-    }).then(function(res) {
-      wx.navigateBack({})
+    }).then(() => {
+      wx.showToast({
+        title: '设置默认地址成功'
+      })
+      this.initShippingAddress()
     })
   },
-
-  addAddess: function() {
+  addAddress() {
     wx.navigateTo({
       url: "/pages/address-add/index"
     })
   },
-
-  editAddess: function(e) {
+  editAddress(e) {
     wx.navigateTo({
       url: "/pages/address-add/index?id=" + e.currentTarget.dataset.id
     })
   },
-
-  onLoad: function() {
-    console.log('onLoad')
-
-
+  onLoad() {
+  
   },
-  onShow: function() {
+  onShow() {
     this.initShippingAddress();
   },
-  initShippingAddress: function() {
-    var that = this;
-    WXAPI.queryAddress(wx.getStorageSync('token')).then(function(res) {
-      if (res.code == 0) {
-        that.setData({
+  initShippingAddress() {
+    WXAPI.queryAddress(wx.getStorageSync('token')).then((res)=> {
+      if (res.code === 0) {
+        this.setData({
           addressList: res.data
         });
-      } else if (res.code == 700) {
-        that.setData({
+      } else if (res.code === 700) {
+        this.setData({
           addressList: null
         });
       }
     })
   }
-
 })
