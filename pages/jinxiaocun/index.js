870d7ec2bd9c43f6f249040c7b93572f652ec927
zhibi <1017866168@qq.com>
2019-07-21 17:50:17 +0800

    增加进销存模块进销记录，增加时间格式化方法

--- pages/jinxiaocun/index.js
+++ pages/jinxiaocun/index.js
@@ -1,11 +1,36 @@
 const WXAPI = require('../../wxapi/main')
+const utils = require('../../utils/index')
 Page({
   data: {
     tabList: ['进销记录', '当前库存'],
-    currentTab: 0
+    currentTab: 0,
+    recordList: [],
+    inventoryTypeMap: [],
+    start: '',
+    end: '',
+    endDate: utils.parseTime(new Date())
   },
-  onLoad: function () {
-  
+  onLoad() {
+    WXAPI.getInventoryList().then(res=>{
+      this.setData({
+        recordList: res.data.list
+      })
+    })
+  },
+  bindDateChange(e){
+    if (e.target.dataset.type === 'start') {
+      this.setData({
+        start: e.detail.value
+      })
+    } else {
+      this.setData({
+        end: e.detail.value
+      })
+    }
+
+  },
+  submit(){
+
   },
   changeCurrentTab(e){
     this.setData({
