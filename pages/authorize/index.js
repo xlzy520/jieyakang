f7d15bc0c63c1c83ee252b2ad61677f1844a8094
zhibi <1017866168@qq.com>
2019-08-26 08:42:00 +0800

    进入该页面说明未注册

--- pages/authorize/index.js
+++ pages/authorize/index.js
@@ -13,38 +13,13 @@ Page({
       return;
     }
     if (app.globalData.isConnected) {
-      wx.setStorageSync('userInfo', e.detail.userInfo)
-      this.login();
+      this.registerUser();
     } else {
       wx.showToast({
         title: '当前无网络',
         icon: 'none',
       })
     }
-  },
-  login() {
-    wx.login({
-      success: res=> {
-        WXAPI.login(res.code).then(res=> {
-          wx.setStorageSync('token', res.data.appToken)
-          if (res.data.isFirst) {
-            this.registerUser();
-            return;
-          }
-          // 回到原来的地方放
-          app.navigateToLogin = false
-          wx.navigateBack();
-        }).catch(err=>{
-          wx.showModal({
-            title: '提示',
-            content: err.msg||'无法登录，请重试',
-            showCancel: false
-          })
-        }).finally(()=>{
-          wx.hideLoading();
-        })
-      }
-    })
   },
   registerUser() {
     wx.login({
